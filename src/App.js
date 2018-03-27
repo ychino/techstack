c0bcb0f34e21127d51c5a0dbf3a3678ba491e7dd
Chino <yuki.chino@bracketglobal.com>
2018-03-27 09:43:44 -0400

    Add Redux

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './reducers'
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
     return (
         <Provider store={createStore(reducers)}>
-            <View />
+            <View style={{ flex: 1 }}>
+                <Header headerText="TechStack" />
+                <LibraryList />
+            </View>
         </Provider>
     );
 };
